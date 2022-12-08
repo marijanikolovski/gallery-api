a224d031cbb4fe3d237985bbc9812453ff3d4d05
marijanikolovski <m.kuzeljevic@gmail.com>
2022-12-08 13:26:52 +0100

    Gallery methods-store, update, destroy

--- app/Http/Controllers/GalleriesController.php
+++ app/Http/Controllers/GalleriesController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Gallery;
+use App\Models\Image;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\CreateGalleryRequest;
+use App\Http\Requests\EditGalleryRequest;
 
 class GalleriesController extends Controller
 {
@@ -19,5 +24,52 @@ class GalleriesController extends Controller
     {
         $gallery = Gallery::with(['images', 'user', 'comments', 'comments.user'])->find($id);
         return response()->json($gallery);
+    }
+
+    public function store(CreateGalleryRequest $request)
+    {
+        $validated = $request->validated();
+
+        $gallry = Gallery::create([
+            'user_id' => Auth::id(),
+            'title' => $validated['title'],
+            'description' => $validated['description']
+        ]);
+
+        $images = $request->get('images', []);
+        foreach ($images as $image) {
+            Image::create([
+                'gallery_id' => $gallry->id,
+                'url' => $image['url']
+            ]);
+        }
+        $gallry->load('images', 'user', 'comments', 'comments.user');
+
+        return response()->json($gallry, 201);
+    }
+
+    public function update($id, EditGalleryRequest $request)
+    {
+        $validated = $request->validated();
+
+        $gallery = Gallery::findOrFail($id);
+        $gallery->update($validated);
+
+        $images = $request->get('images', []);
+        foreach ($images as $image) {
+            $imagesArr[] = Image::create([
+                'gallery_id' => $gallery->id,
+                'url' => $image['url']
+            ]);
+        }
+        $gallery->load('images', 'user', 'comments', 'comments.user');
+        return response()->json($gallery);
+    }
+
+    public function destroy($id)
+    {
+        $gallery = Gallery::findOrFail($id);
+        $gallery->delete();
+        return response()->noContent();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -34,5 +34,8 @@ Route::controller(GalleriesController::class)->group(
     function () {
         Route::get('/galleries', 'index');
         Route::get('/galleries/{id}', 'show');
+        Route::post('/create', 'store');
+        Route::put('/galleries/{id}', 'update');
+        Route::delete('/galleries/{id}', 'destroy');
     }
 );
